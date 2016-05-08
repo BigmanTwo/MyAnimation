2feb166a75c7e763ba7ffc2da20f9c6e72007860
Asus <347253494@qq.com>
2016-05-08 11:57:25 +0800

    layoutAnimation代码加载

--- app/src/main/java/com/example/asus/myanimation/TransitonActivity.java
+++ app/src/main/java/com/example/asus/myanimation/TransitonActivity.java
@@ -3,6 +3,9 @@ package com.example.asus.myanimation;
 import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -22,7 +25,15 @@ public class TransitonActivity extends AppCompatActivity {
             mList.add("数据添加"+i);
         }
         ArrayAdapter adapter=new ArrayAdapter(this,R.layout.support_simple_spinner_dropdown_item,mList);
+//        setAnimation(adapter);
+    }
+//代码加载layoutAnimation
+    private void setAnimation(ArrayAdapter adapter) {
         mListView.setAdapter(adapter);
-
+        Animation animation= AnimationUtils.loadAnimation(this, R.anim.traslat);
+        LayoutAnimationController ctr=new LayoutAnimationController(animation);
+        ctr.setOrder(LayoutAnimationController.ORDER_NORMAL);
+        ctr.setDelay(0.7f);
+        mListView.setLayoutAnimation(ctr);
     }
 }
